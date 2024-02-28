445f0ab536267158346a2d36f7fa5994c4b5dbe2
Djivan <u71536@gmail.com>
2024-02-28 22:32:01 +0300

    fixes

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -4,6 +4,7 @@
 # Вася: 1
 # Маша: 2
 # Петя: 2
+print(f'Задание 1')
 
 students = [
     {'first_name': 'Вася'},
@@ -12,13 +13,25 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+
+names = dict()
+
+for student in students:
+    if student['first_name'] not in names.keys():
+        names[student['first_name']] = 1
+    else:
+        names[student['first_name']] += 1
+
+for name, count in names.items():
+    print(f'{name}: {count}')
 
 
 # Задание 2
 # Дан список учеников, нужно вывести самое часто повторящееся имя
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
+print(f'\nЗадание 2')
+
 students = [
     {'first_name': 'Вася'},
     {'first_name': 'Петя'},
@@ -26,7 +39,18 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+
+names = dict()
+
+for student in students:
+    if student['first_name'] not in names.keys():
+        names[student['first_name']] = 1
+    else:
+        names[student['first_name']] += 1
+
+max_name = max(names, key=names.get)
+
+print(f'Caмое частое имя среди учеников: {max_name}')
 
 
 # Задание 3
@@ -34,6 +58,7 @@ students = [
 # Пример вывода:
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
+print(f'\nЗадание 3')
 
 school_students = [
     [  # это – первый класс
@@ -51,7 +76,19 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+names = dict()
+i = 1
+
+for student_list in school_students:
+    for student in student_list:
+        if student['first_name'] not in names.keys():
+            names[student['first_name']] = 1
+        else:
+            names[student['first_name']] += 1
+    max_name = max(names, key=names.get)
+    print(f'Caмое частое имя среди учеников в классе {i}: {max_name}')
+    i += 1
+    names = dict()
 
 
 # Задание 4
@@ -59,6 +96,7 @@ school_students = [
 # Пример вывода:
 # Класс 2a: девочки 2, мальчики 0 
 # Класс 2б: девочки 0, мальчики 2
+print(f'\nЗадание 4')
 
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
@@ -72,7 +110,30 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
+
+i = 0
+girls = 0
+boys = 0
+
+
+for school_student in school:
+    one_class = school[i]
+    k = 0
+    for j in school_student["students"]:
+        class_names = one_class['students']
+        class_first_name = class_names[k]
+        student_name = class_first_name['first_name']
+        if student_name in is_male.keys():
+            x = is_male.get(student_name)
+            if x is False:
+                girls += 1
+            else:
+                boys += 1
+        k += 1
+    print(f'Класс {school[i]["class"]}: девочки {girls}, мальчики {boys}')
+    boys = 0
+    girls = 0
+    i += 1
 
 
 # Задание 5
@@ -80,6 +141,7 @@ is_male = {
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
+print(f'\nЗадание 4')
 
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
@@ -91,5 +153,30 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
 
+i = 0
+girls = 0
+boys = 0
+
+
+for school_student in school:
+    one_class = school[i]
+    k = 0
+    for j in school_student["students"]:
+        class_names = one_class['students']
+        class_first_name = class_names[k]
+        student_name = class_first_name['first_name']
+        if student_name in is_male.keys():
+            x = is_male.get(student_name)
+            if x is False:
+                girls += 1
+            else:
+                boys += 1
+        k += 1
+    if boys > girls:
+        print(f'Больше всего мальчиков в классе {school[i]["class"]}')
+    else:
+        print(f'Больше всего девочек в классе {school[i]["class"]}')
+    boys = 0
+    girls = 0
+    i += 1
